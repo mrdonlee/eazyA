3ca815601a139f7cea0aeb03474bec936de111e0
Don Lee <git@mrdonlee.addy.io>
2025-04-20 02:29:55 +0100

    feat: add GCP dataframe creator

--- data_model/src/data_model/utils/dataframe_converter.py
+++ data_model/src/data_model/utils/dataframe_converter.py
@@ -1,7 +1,29 @@
+from google.cloud import storage
 import pandas as pd
+import io
 
 
-def convert_to_dataframe(api_data):
+def download_blob(source_blob_name, bucket_name="polkadot-dataset-bucket"):
+    """
+    Download a blob from the bucket and return its content as bytes.
+
+    Parameters:
+    bucket_name (str): The name of the bucket.
+    source_blob_name (str): The name of the blob to download.
+
+    Returns:
+    bytes: The content of the blob.
+    """
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+
+    # Download the blob's content as bytes
+    return blob.download_as_bytes()
+
+
+def convert_to_dataframe(blob_uri):
     """
     Convert raw API data into a pandas DataFrame.
 
@@ -11,4 +33,7 @@ def convert_to_dataframe(api_data):
     Returns:
     pd.DataFrame: A DataFrame containing the API data.
     """
+    api_data = download_blob(source_blob_name=blob_uri)
+    api_data = pd.read_json(io.BytesIO(api_data), lines=True)
+
     return pd.DataFrame(api_data)
