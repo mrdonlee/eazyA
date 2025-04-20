4b1f38badaca53185426fbaece1f922de9beee2f
Anas <manasgithub1@gmail.com>
2025-04-20 11:38:51 +0100

    feat: polkadot api

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import dynamic from 'next/dynamic'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { MainNav } from '@/components/main-nav'
+import { UserNav } from '@/components/user-nav'
 
 export const inter = Inter({
     subsets: ['latin'],
@@ -23,10 +25,20 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${inter.variable} font-inter antialiased`}>
-                <QueryClientProvider client={queryClient}>
-                    <PolkadotProvider>{children}</PolkadotProvider>
-                </QueryClientProvider>
+            <body className={`${inter.variable} font-inter antialiased dark`}>
+                <header className="sticky top-0 z-50 border-b bg-background">
+                    <div className="flex h-16 items-center px-4 md:px-6">
+                        <MainNav />
+                        <div className="ml-auto flex items-center space-x-4">
+                            <UserNav />
+                        </div>
+                    </div>
+                </header>
+                <main className="flex-1">
+                    <QueryClientProvider client={queryClient}>
+                        <PolkadotProvider>{children}</PolkadotProvider>
+                    </QueryClientProvider>
+                </main>
             </body>
         </html>
     )
